c1a911a25d2e343a724a20f6d9157efca0c3d58f
BugHunter-X <askzenai@gmail.com>
2025-01-16 10:56:06 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<int> vec;
+  for (int i = 0; i < 10; ++i) {
+    vec.push_back(i);
+  }
+
+  // Correct way to erase an element using iterators
+  for (auto it = vec.begin(); it != vec.end(); ) {
+    if (*it == 5) {
+      it = vec.erase(it);
+    } else {
+      ++it;
+    }
+  }
+
+  for (int i = 0; i < vec.size(); ++i) {
+    std::cout << vec[i] << " ";
+  }
+  std::cout << std::endl;
+  return 0;
+}
